09f5b8ba9679564ea1ece97c5aba6a3a89fd5660
rp <rp@burgeins.de>
2023-02-21 15:51:38 +0100

    * styled the countdown * added a method to write the value append with the singular or plural word

--- src/App.svelte
+++ src/App.svelte
@@ -7,23 +7,31 @@
     function handleFinishEvent() {
         done = true;
     }
+
+    function format(value, singular, plural) {
+        if (value === 1) {
+            return `${value} ${singular}`;
+        }
+
+        return `${value} ${plural}`;
+    }
 </script>
 
 <div class="d-flex flex-column justify-content-center align-items-center min-vw-100 min-vh-100 position-relative">
     {#if done}
         <BirthdayCard/>
     {:else}
-        <TimerWall to="2023-02-21T15:35:00+0100" let:remaining on:finish={handleFinishEvent}>
-            <div class="position-absolute top-0 start-0 vw-100 vh-100 bg-black text-white">
+        <TimerWall to="2023-02-22T18:00:00+0100" let:remaining on:finish={handleFinishEvent}>
+            <h1 class="position-absolute top-0 start-0 vw-100 vh-100 bg-black text-white d-flex justify-content-center align-items-center">
                 {#if remaining.done === false}
-                    <span>{remaining.days} Tage</span>
-                    <span>{remaining.hours} Stunden</span>
-                    <span>{remaining.minutes} Minuten</span>
-                    <span>{remaining.seconds} Sekunden</span>
+                    {format(remaining.days, 'Tag', 'Tage')},
+                    {format(remaining.hours, 'Stunde', 'Stunden')},
+                    {format(remaining.minutes, 'Minute', 'Minuten')},
+                    {format(remaining.seconds, 'Sekunde', 'Sekunden')}
                 {:else}
                     <h2>The time has come!</h2>
                 {/if}
-            </div>
+            </h1>
         </TimerWall>
     {/if}
 </div>
